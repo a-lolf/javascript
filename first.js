9409b7b2cce0cd8661a8ced5e4a382f76e08a8b0
Amaan <ansari.amaan@outlook.com>
2024-08-21 02:37:45 +0530

    object completed

--- first.js
+++ first.js
@@ -1,25 +1,22 @@
 console.log("hello world");
 
-let username = "amaan"
+let username = "amaan";
 let userOne = {
-    "email": "amaan@email.com",
-    "phone": 870787
-
-}
+  email: "amaan@email.com",
+  phone: 870787,
+};
 
 console.log(userOne.email);
 
-let userTwo = userOne
+let userTwo = userOne;
 
-userTwo.email = "ansari@email.com"
+userTwo.email = "ansari@email.com";
 
 console.log(userTwo, userOne);
 
+let marvel_heros = ["Ironman", "Spiderman", "Hulk"];
 
-let marvel_heros = ["Ironman", "Spiderman", "Hulk"]
-
-let dc_heros = ["Batman", "Superman", "Flash"]
-
+let dc_heros = ["Batman", "Superman", "Flash"];
 
 console.log(...marvel_heros, ...dc_heros);
 console.log([...marvel_heros, ...dc_heros]);
@@ -27,3 +24,34 @@ console.log([...marvel_heros, ...dc_heros]);
 console.log(marvel_heros.concat(dc_heros));
 
 console.log(Array.from("abcd"));
+
+let obj1 = {
+  1: "a",
+  2: {
+    22: "2a",
+  },
+};
+
+let obj2 = {
+  name: "amaan",
+  age: 24,
+  details: {
+    profession: "SDE",
+    more_details: {
+      which_dev: "backend",
+    },
+  },
+};
+
+console.log(obj2.details.more_details.which_dev);
+
+
+const course = {
+    coursename: "js in hindi",
+    price: 999,
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
