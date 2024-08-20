d595aa2d6c84b662329c978f191636d8d6828239
a-lolf <111639521+a-lolf@users.noreply.github.com>
2024-08-20 20:36:31 +0000

    modified:   first.js

--- first.js
+++ first.js
@@ -1,1 +1,29 @@
 console.log("hello world");
+
+let username = "amaan"
+let userOne = {
+    "email": "amaan@email.com",
+    "phone": 870787
+
+}
+
+console.log(userOne.email);
+
+let userTwo = userOne
+
+userTwo.email = "ansari@email.com"
+
+console.log(userTwo, userOne);
+
+
+let marvel_heros = ["Ironman", "Spiderman", "Hulk"]
+
+let dc_heros = ["Batman", "Superman", "Flash"]
+
+
+console.log(...marvel_heros, ...dc_heros);
+console.log([...marvel_heros, ...dc_heros]);
+
+console.log(marvel_heros.concat(dc_heros));
+
+console.log(Array.from("abcd"));
